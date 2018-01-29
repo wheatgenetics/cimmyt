4cd0a020fd7a52cb6c0919dd6bb5a977738441fe
mlucas <mark.lucas.cork@gmail.com>
2018-01-29 13:52:48 +1100

    Initial Version

--- Utilities/populate_phenotypes_from_database_file.py
+++ Utilities/populate_phenotypes_from_database_file.py
@@ -5,13 +5,15 @@
 #
 # Program to load CIMMYT phenotypes table from CIMMYT database file.
 #
+# N.B. Check order of columns in input file as it is not consistently the same!!
+#
 #
 
 from pyexcel_xlsx import get_data
 import sys
 import mysql.connector
 from mysql.connector import errorcode
-import config
+import test_config
 import argparse
 
 # Get command line input.
@@ -19,10 +21,12 @@ import argparse
 cmdline = argparse.ArgumentParser()
 
 cmdline.add_argument('-f', '--file', help='Full Path to CIMMYT database input file to import')
+cmdline.add_argument('-s', '--sheet', help='Name of Excel Worksheet to Load')
 
 args = cmdline.parse_args()
 
 inputFile = args.file
+wksheet=args.sheet
 
 phenotypeRecord = []
 phenotypeList = []
@@ -31,29 +35,29 @@ phenotypeList = []
 data = get_data(inputFile)
 
 index = 0
-for item in data['Sheet1']:
+for item in data[wksheet]:
     if index == 0:
         pass
     elif index > 0:
-        plotId=data['Sheet1'][index][0]
-        iyear=data['Sheet1'][index][1]
-        ilocation=data['Sheet1'][index][2]
-        itrial=data['Sheet1'][index][3]
-        icondition=data['Sheet1'][index][4]
-        occ =data['Sheet1'][index][5]
-        trial=data['Sheet1'][index][6]
-        cid=data['Sheet1'][index][7]
-        sid = data['Sheet1'][index][8]
-        gid = data['Sheet1'][index][9]
-        crossName= data['Sheet1'][index][10]
-        selectionHistory = data['Sheet1'][index][11]
-        origin = data['Sheet1'][index][12]
-        plot = str(data['Sheet1'][index][13])
-        rep = str(data['Sheet1'][index][14])
-        block = str(data['Sheet1'][index][15])
-        entry = str(data['Sheet1'][index][16])
-        dateStr=str(data['Sheet1'][index][17])
-        if len(dateStr) > 0:
+        plotId=data[wksheet][index][0]
+        iyear=data[wksheet][index][1]
+        ilocation=data[wksheet][index][2]
+        itrial=data[wksheet][index][3]
+        icondition=data[wksheet][index][4]
+        occ =data[wksheet][index][5]
+        trial=data[wksheet][index][6]
+        cid=data[wksheet][index][7]
+        sid = data[wksheet][index][8]
+        gid = data[wksheet][index][9]
+        crossName= data[wksheet][index][10]
+        selectionHistory = data[wksheet][index][11]
+        origin = data[wksheet][index][12]
+        plot = str(data[wksheet][index][13])
+        rep = str(data[wksheet][index][14])
+        block = str(data[wksheet][index][15])
+        entry = str(data[wksheet][index][16])
+        dateStr=str(data[wksheet][index][17])
+        if len(dateStr) > 0 and dateStr !='000000':
             year='20'+ dateStr[0:2]
             month = dateStr[2:4]
             day = dateStr[4:6]
@@ -61,20 +65,21 @@ for item in data['Sheet1']:
         else:
             phenoDate=None
 
-        phenoValue=data['Sheet1'][index][18]
-        traitId=str(data['Sheet1'][index][19])
+        phenoValue=data[wksheet][index][19]
+        traitId=str(data[wksheet][index][18])
         phenoPerson=None
         phenotypeRecord=[plotId,iyear,ilocation,itrial,icondition,plot,traitId,phenoValue,phenoDate,phenoPerson]
         phenotypeList.append(phenotypeRecord)
 
     index+=1
+    print(index)
 
 print("")
 print("Connecting to Database...")
 
 try:
-    cnx = mysql.connector.connect(user=config.USER, password=config.PASSWORD, host=config.HOST,
-                                      port=config.PORT,database=config.DATABASE)
+    cnx = mysql.connector.connect(user=test_config.USER, password=test_config.PASSWORD, host=test_config.HOST,
+                                      port=test_config.PORT,database=test_config.DATABASE)
 except mysql.connector.Error as err:
     if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
         print("Something is wrong with your user name or password")
